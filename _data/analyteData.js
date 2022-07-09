ee2fc747c430b173f9c59ba0d2e7dd5a1b955ab6
Jason Mortensen <ingrey1@gmail.com>
2022-07-09 16:10:58 -0700

    oooo-so-cool

--- _data/analyteData.js
+++ _data/analyteData.js
@@ -6,21 +6,22 @@ module.exports = function () {
     metadata: {
       tableHeaderName: "RETRIEVED ANALYTE",
     },
-    data: {
-      firstAnalyte: {
+    data: [
+      {
+        id: 1,
         name: "First Analyte",
         row1: ["col1Value", "col2Value"],
         row2: ["col1Value", "col2Value"],
         row3: ["col1Value", "col2Value"],
         row4: ["col1Value", "col2Value"],
       },
-      secondAnalyte: {
+      {
         // all analyte data objects have same structure
       },
       // we could store a limited number of analytes here this way
       // we dont want all the ones in the excel file if it has like 10,000
       // when the user clicks search after typing "firstAnalyte" in box 
-    },
+    ],
   };
 
   return replaceMeWithRealAnalytes;
